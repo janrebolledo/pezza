2b8a67ecda703aaf40aaa4a9136fc13f7680a475
janrebolledo <janrebolledo63@gmail.com>
2022-08-06 21:59:58 -0700

    Portfolio Page

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import React, { useState } from "react";
 
+import BasedIn from "../../public/based in.png";
+import Turnarounds from "../../public/turnarounds.png";
+
 function Header() {
   const [headerVisible, setHeaderVisible] = useState(false);
   return (
     <header className="w-full h-[13vh] z-[100000000000000000] bg-bb text-white fixed flex flex-row justify-between items-center px-5 shadow-lg">
       <h1 className="text-3xl drop-shadow-lg">Pezza VFX</h1>
-      <div></div>
-      <p className="btn">
-        <Link href="/">Contact &rarr;</Link>
-      </p>
+      <img className="w-48" src={BasedIn.src} />
+      <div className="flex flex-row gap-6">
+        <Link href="/">HOME</Link>
+        <Link href="/about">ABOUT</Link>
+        <Link href="/portfolio">PORTFOLIO</Link>
+      </div>
+      <img className="w-48" src={Turnarounds.src} />
+      <Link href="/">CONTACT &rarr;</Link>
     </header>
   );
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,5 +1,14 @@
 import React from "react";
-import Head from "next/link";
+import Head from "next/head";
+import Link from "next/link";
+import Contact from "../components/Layout/Contact";
+
+import { Navigation, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
 
 const metadata = {
   title: "Pezza VFX — Portfolio",
@@ -8,9 +17,39 @@ const metadata = {
     "pezza vfx, video production, content strategy, photography, videography, pezza vfx portfolio",
 };
 
+const categories = [
+  {
+    name: "Video Editing",
+    description:
+      "Delivering high quality videos. Creating corprate videos to sports highlights & theatrical trailers..",
+    link: "/projects/video-editing",
+  },
+  {
+    name: "Videography",
+    description:
+      "Videography that engages your audience and builds strength in your brand.",
+    link: "/projects/videography",
+  },
+  {
+    name: "Photography",
+    description: "Quality photography that builds trust with your clients.",
+    link: "/projects/photography",
+  },
+  {
+    name: "Social Media Management",
+    description: "Content strategy that builds communities.",
+    link: "/projects/social-media-management",
+  },
+  {
+    name: "Motion Graphics",
+    description: "Premium motion graphics that grab your audience's attention.",
+    link: "/projects/motion-design",
+  },
+];
+
 function portfolio() {
   return (
-    <main>
+    <main className="text-white">
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
@@ -22,8 +61,50 @@ function portfolio() {
 
         <meta property="twitter:card" content="summary_large_image" />
       </Head>
-      <div>portfolio</div>
+      <section className="pt-[15vh]">
+        <h1 className="text-center text-white text-7xl my-8">Portfolio</h1>
+        <Swiper
+          spaceBetween={32}
+          slidesPerView={1.5}
+          navigation
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          modules={[Navigation, Autoplay]}
+        >
+          {categories.map((category, index) => (
+            <SwiperSlide key={index}>
+              <CategorySlide category={category} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+
+      <section className="px-5">
+        <h1 className="text-7xl">Recent Clients</h1>
+      </section>
+
+      <Contact />
     </main>
+  );
+}
+
+function CategorySlide({ category }) {
+  const { name, description, link } = category;
+  return (
+    <div className="relative w-full">
+      <div className="p-5 absolute top-0 h-full w-full justify-end flex flex-col gap-4">
+        <h1 className="text-5xl">{name}</h1>
+        <p>{description}</p>
+        <Link href={link}>
+          <p className="btn">View {name} Projects &rarr;</p>
+        </Link>
+      </div>
+      <img className="w-full h-[492px] bg-white/50" />
+    </div>
   );
 }
 
