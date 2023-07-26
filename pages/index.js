03c01c2e956310963b014d92d8595953f05dca28
Jan <55969944+janrebolledo@users.noreply.github.com>
2023-07-25 23:42:55 -0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
-import { createClient } from "contentful";
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import { createClient } from 'contentful';
 
-import Section from "../public/section.svg";
-import ProjectsList from "../components/Projects/ProjectsList";
-import ContactSection from "../components/Layout/ContactSection";
-import Testimonials from "../components/Layout/Testimonials";
+import Section from '../public/section.svg';
+import ProjectsList from '../components/Projects/ProjectsList';
+import ContactSection from '../components/Layout/ContactSection';
+import Testimonials from '../components/Layout/Testimonials';
 
-import VisualEffects from "../public/visual-effects.png";
+import VisualEffects from '../public/visual-effects.png';
 
 const metadata = {
-  title: "Pezza VFX",
-  description: "CUTTING EDGE PRODUCTION THAT BUILDS BRANDS",
+  title: 'Pezza VFX',
+  description: 'CUTTING EDGE PRODUCTION THAT BUILDS BRANDS',
   keywords:
-    "pezza vfx, video production, content strategy, photography, videography",
+    'pezza vfx, video production, content strategy, photography, videography',
 };
 
 export default function Home({ projects, testimonials, copy }) {
@@ -26,41 +26,41 @@ export default function Home({ projects, testimonials, copy }) {
   ];
 
   return (
-    <main className="text-white pt-[13vh] md:pt-0">
+    <main className='text-white pt-[13vh] md:pt-0'>
       <Head>
         <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords} />
+        <meta name='description' content={metadata.description} />
+        <meta name='keywords' content={metadata.keywords} />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={metadata.title} />
+        <meta property='og:description' content={metadata.description} />
 
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property='twitter:card' content='summary_large_image' />
       </Head>
 
-      <section className="w-full md:min-h-screen grid md:grid-cols-2 pt-[13vh] gap-4 px-5">
-        <div className="w-full flex flex-col items-start justify-center">
-          <h1 className="text-5xl md:text-7xl whitespace-pre-wrap">
+      <section className='w-full md:min-h-screen grid md:grid-cols-2 pt-[13vh] gap-4 px-5'>
+        <div className='w-full flex flex-col items-start justify-center'>
+          <h1 className='text-5xl md:text-7xl whitespace-pre-wrap'>
             {copy.landingPage}
           </h1>
-          <div className="flex flex-row gap-4">
-            <p className="btn">
-              <Link href="/contact">Contact &rarr;</Link>
+          <div className='flex flex-row gap-4'>
+            <p className='btn'>
+              <Link href='/contact'>Contact &rarr;</Link>
             </p>
-            <p className="btns">
-              <Link href="/portfolio">Explore Projects &rarr;</Link>
+            <p className='btns'>
+              <Link href='/portfolio'>Explore Projects &rarr;</Link>
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-center p-8 md:p-0">
-          <Link href={copy.landingPageVideo1Link || ""}>
+        <div className='flex items-center justify-center p-8 md:p-0'>
+          <Link href={copy.landingPageVideo1Link || ''}>
             <video
               autoPlay
               muted
               loop
               playsInline
-              className="aspect-video shadow-lg rounded-xl"
+              className='aspect-video shadow-lg rounded-xl'
             >
               <source src={copy.landingPageVideo1.fields.file.url} />
             </video>
@@ -68,65 +68,65 @@ export default function Home({ projects, testimonials, copy }) {
         </div>
       </section>
 
-      <section className="w-full px-5 py-16">
-        <h1 className="text-5xl md:text-7xl mb-4 shape rect-f-l">
+      <section className='w-full px-5 py-16'>
+        <h1 className='text-5xl md:text-7xl mb-4 shape rect-f-l'>
           FEATURED WORK
         </h1>
         <ProjectsList projects={recentProjects} />
-        <div className="w-full flex justify-center">
-          <p className="btn mt-4">
-            <Link href="/portfolio">View All &rarr;</Link>
+        <div className='w-full flex justify-center'>
+          <p className='btn mt-4'>
+            <Link href='/portfolio'>View All &rarr;</Link>
           </p>
         </div>
       </section>
 
-      <section className="relative">
-        <img src={Section.src} className="hidden xl:block w-full" alt="" />
-        <div className="xl:absolute top-0 w-full h-full p-5 py-12 lg:py-5 grid items-center lg:grid-cols-4 gap-8 bgradient xl:bg-none">
+      <section className='relative'>
+        <img src={Section.src} className='hidden xl:block w-full' alt='' />
+        <div className='xl:absolute top-0 w-full h-full p-5 py-12 lg:py-5 grid items-center lg:grid-cols-4 gap-8 bgradient xl:bg-none'>
           <div>
-            <h1 className="text-5xl md:text-7xl shape tri-d">Services</h1>
+            <h1 className='text-5xl md:text-7xl shape tri-d'>Services</h1>
             <p>{copy.servicesParagraph}</p>
-            <p className="btnt mt-4">
-              <Link href="/portfolio">View All &rarr;</Link>
+            <p className='btnt mt-4'>
+              <Link href='/portfolio'>View All &rarr;</Link>
             </p>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <h2 className="flex items-center gap-4 text-3xl">
-              <span className="material-symbols-outlined !text-5xl">cut</span>
+          <div className='flex flex-col gap-4'>
+            <h2 className='flex items-center gap-4 text-3xl'>
+              <span className='material-symbols-outlined !text-5xl'>cut</span>
               {copy.serviceHeading1}
             </h2>
             <p>{copy.serviceParagraph1}</p>
-            <Link href="/projects/video-editing">
-              <p className="btns">
+            <Link href='/projects/video-editing'>
+              <p className='btns'>
                 View {copy.serviceHeading1} Projects &rarr;
               </p>
             </Link>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <h2 className="flex items-center gap-4 text-3xl">
-              <span className="material-symbols-outlined !text-5xl">
+          <div className='flex flex-col gap-4'>
+            <h2 className='flex items-center gap-4 text-3xl'>
+              <span className='material-symbols-outlined !text-5xl'>
                 videocam
               </span>
               {copy.serviceHeading2}
             </h2>
             <p>{copy.serviceParagraph2}</p>
-            <Link href="/projects/videography">
-              <p className="btns">
+            <Link href='/projects/videography'>
+              <p className='btns'>
                 View {copy.serviceHeading2} Projects &rarr;
               </p>
             </Link>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <h2 className="flex items-center gap-4 text-3xl">
-              <Image src={VisualEffects} width={48} height={48} />
+          <div className='flex flex-col gap-4'>
+            <h2 className='flex items-center gap-4 text-3xl'>
+              <Image src={VisualEffects} width={48} height={48} alt />
               {copy.serviceHeading3}
             </h2>
             <p>{copy.serviceParagraph3}</p>
-            <Link href="/projects/visual-effects">
-              <p className="btns">
+            <Link href='/projects/visual-effects'>
+              <p className='btns'>
                 View {copy.serviceHeading3} Projects &rarr;
               </p>
             </Link>
@@ -147,9 +147,9 @@ export async function getStaticProps() {
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
-  const res = await client.getEntries({ content_type: "project" });
-  const res1 = await client.getEntry("3YyunYoPT2MlpeAMRT6w0F");
-  const res2 = await client.getEntries({ content_type: "testimonial" });
+  const res = await client.getEntries({ content_type: 'project' });
+  const res1 = await client.getEntry('3YyunYoPT2MlpeAMRT6w0F');
+  const res2 = await client.getEntries({ content_type: 'testimonial' });
 
   return {
     props: {
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,9 +1,9 @@
-import React from "react";
-import Head from "next/head";
-import { createClient } from "contentful";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
-import Link from "next/link";
+import React from 'react';
+import Head from 'next/head';
+import { createClient } from 'contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+import Link from 'next/link';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -11,7 +11,7 @@ const client = createClient({
 });
 
 export const getStaticPaths = async () => {
-  const res = await client.getEntries({ content_type: "project" });
+  const res = await client.getEntries({ content_type: 'project' });
 
   const paths = res.items.map((item) => {
     return {
@@ -29,8 +29,8 @@ export const getStaticPaths = async () => {
 
 export async function getStaticProps({ params }) {
   const { items } = await client.getEntries({
-    content_type: "project",
-    "fields.slug": params.slug,
+    content_type: 'project',
+    'fields.slug': params.slug,
   });
 
   return {
@@ -38,9 +38,9 @@ export async function getStaticProps({ params }) {
   };
 }
 
-const Bold = ({ children }) => <p className="bold">{children}</p>;
+const Bold = ({ children }) => <p className='bold'>{children}</p>;
 
-const Heading = ({ children }) => <h1 className="text-7xl">{children}</h1>;
+const Heading = ({ children }) => <h1 className='text-7xl'>{children}</h1>;
 
 const options = {
   renderNode: {
@@ -53,37 +53,37 @@ function ProjectPage({ project }) {
     project.fields;
 
   return (
-    <main className="text-white">
+    <main className='text-white'>
       <Head>
-        <title>{"Pezza VFX — " + title}</title>
-        <meta name="description" content={excerpt} />
+        <title>{'Pezza VFX — ' + title}</title>
+        <meta name='description' content={excerpt} />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={"Pezza VFX — " + title} />
-        <meta property="og:description" content={excerpt} />
-        <meta property="og:image" content={"https:" + image.fields.file.url} />
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={'Pezza VFX — ' + title} />
+        <meta property='og:description' content={excerpt} />
+        <meta property='og:image' content={'https:' + image.fields.file.url} />
+        <meta property='twitter:card' content='summary_large_image' />
         <meta
-          property="twitter:image"
-          content={"https:" + image.fields.file.url}
+          property='twitter:image'
+          content={'https:' + image.fields.file.url}
         />
       </Head>
 
-      <section className="p-5 pt-[15vh] h-full bgradient">
-        <div className="flex flex-row justify-between items-center my-8">
-          <h1 className="text-7xl">{title} - CLIENT OVERVIEW</h1>
+      <section className='p-5 pt-[15vh] h-full bgradient'>
+        <div className='flex flex-row justify-between items-center my-8'>
+          <h1 className='text-7xl'>{title} - CLIENT OVERVIEW</h1>
           <p>{date}</p>
         </div>
 
-        <section className="pb-8 text-lg">
-          {documentToReactComponents(content, options)}
+        <section className='pb-8 text-lg'>
+          {/* {documentToReactComponents(content, options)} */}
         </section>
 
         {videoEmbed != undefined ? (
-          <div className="flex w-full justify-center">
+          <div className='flex w-full justify-center'>
             <div
-              dangerouslySetInnerHTML={{ __html: videoEmbed }}
-              className="videoEmbed 2xl:w-[50%]"
+              dangerouslySetInnerHTML={{ __html: videoEmbed + '</iframe>' }}
+              className='videoEmbed 2xl:w-[50%]'
             />
           </div>
         ) : (
@@ -93,29 +93,29 @@ function ProjectPage({ project }) {
             )}
           </>
         )}
-        {category[0].includes("Photography") && (
-          <p className="btns mt-8">
-            <Link href="/projects/photography">&larr; RETURN</Link>
+        {category[0].includes('Photography') && (
+          <p className='btns mt-8'>
+            <Link href='/projects/photography'>&larr; RETURN</Link>
           </p>
         )}
-        {category[0].includes("Video Editing") && (
-          <p className="btns mt-8">
-            <Link href="/projects/video-editing">&larr; RETURN</Link>
+        {category[0].includes('Video Editing') && (
+          <p className='btns mt-8'>
+            <Link href='/projects/video-editing'>&larr; RETURN</Link>
           </p>
         )}
-        {category[0].includes("Videography") && (
-          <p className="btns mt-8">
-            <Link href="/projects/videography">&larr; RETURN</Link>
+        {category[0].includes('Videography') && (
+          <p className='btns mt-8'>
+            <Link href='/projects/videography'>&larr; RETURN</Link>
           </p>
         )}
-        {category[0].includes("Social Media Management") && (
-          <p className="btns mt-8">
-            <Link href="/projects/social-media-management">&larr; RETURN</Link>
+        {category[0].includes('Social Media Management') && (
+          <p className='btns mt-8'>
+            <Link href='/projects/social-media-management'>&larr; RETURN</Link>
           </p>
         )}
-        {category[0].includes("Visual Effects") && (
-          <p className="btns mt-8">
-            <Link href="/projects/visual-effects">&larr; RETURN</Link>
+        {category[0].includes('Visual Effects') && (
+          <p className='btns mt-8'>
+            <Link href='/projects/visual-effects'>&larr; RETURN</Link>
           </p>
         )}
       </section>
